90d72907ca18e32b014c5b63b8663935f00e14e9
Andrei Dmytruk <mallboroks@mail.ru>
2016-09-04 20:11:40 +0300

    1-2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function myResult (base, exponent) {
+  var base=prompt('Введите число:');
+  var exponent=prompt('Введите степень умножения:');
+  console.log( Math.pow(base, exponent) );
+}
+
+myResult ();
