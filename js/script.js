77ccc3390cd3b1fec8238214e5a6e75a1266d27b
Andrei Dmytruk <mallboroks@mail.ru>
2016-09-04 20:17:41 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-function myResult (base, exponent) {
+function myResult () {
   var base=prompt('Введите число:');
   var exponent=prompt('Введите степень умножения:');
   console.log( Math.pow(base, exponent) );
